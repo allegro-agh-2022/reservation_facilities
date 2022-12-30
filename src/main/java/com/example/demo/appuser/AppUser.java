548329e52f8aff6260644ab011fa0c94611ea0c5
gabi15 <gabcia100@gmail.com>
2022-12-30 21:12:19 +0100

    add malfunctions

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -1,6 +1,9 @@
 package com.example.demo.appuser;
 
+import com.example.demo.malfunction.Malfunction;
 import com.example.demo.reservation.Reservation;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,10 +29,17 @@ public class AppUser {
     private String name;
     private String surname;
     private String email;
+
+    @JsonIgnore
     private String password;
 
     @ManyToMany(fetch = EAGER)
     private Collection<Role> roles = new ArrayList<>();
+
+    @JsonBackReference
+    @OneToMany(mappedBy="appUser", cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<Malfunction> malfunctions = new ArrayList<>();
 
     @JsonManagedReference
     @OneToMany(mappedBy = "appUser",
---
+++ src/main/java/com/example/demo/malfunction/MalfunctionForm.java
@@ -0,0 +1,5 @@
+package com.example.demo.malfunction;
+
+public record MalfunctionForm(String description, String title, String roomName) {
+
+}
